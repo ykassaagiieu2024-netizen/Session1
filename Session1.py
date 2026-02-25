97dbb466d73e2dfeaa6fd4f8f138d6138d666016
yasminekassaagi <ykassaagi.ieu2024@student.ie.edu>
2026-02-25 16:04:29 +0100

    First session of programming !

---
+++ Session1.py
@@ -0,0 +1,19 @@
+# This is our first python code
+# We will try several print examples
+print("Hello World")
+# print('Hello World')
+#print("2+5")
+print(2+5)
+print('"'"'")
+print("\"'") # Escapes (we will talk abt it later)
+# You can print multiple things using the comma separator
+print("2+3+4+5=", 2+3+4+5)
+print("my age is", 10+9, "and I am", 100+73, "cm tall")
+# Commas translate to a space when printing stuff
+# We can change how parameters are spaced out
+print("my age is", 40, sep="bob")
+print(1,2,3,4,40, sep="bob", end="||")
+print("my age is: ", 10+9, ". I am: ", 100+73, "cm tall.", sep="")
+print(1,2,3,4,40, sep="bob", end="\n\n\n")
+
+print("hello world again")
